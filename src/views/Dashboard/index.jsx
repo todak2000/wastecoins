29ecec7f2df22b900f5bf7b12491dfead7d26096
todak2000 <todak2000@gmail.com>
2020-05-04 04:34:56 +0100

    feat(Dashboard) template header and footer correction7

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import UserHeader from '../../components/UserHeader/index';
 import UserSidebar from '../../components/UserSidebar/index';
 import Back_Button from '../../images/Chevron.svg';
+import Footer from '../../components/Footer/index'
 import './dashboard.css';
 
 function Dashboard() {
