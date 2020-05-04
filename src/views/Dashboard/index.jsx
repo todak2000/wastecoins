eb1c546cc80627cbe6ad133bc7e6fd9b7a8c5828
todak2000 <todak2000@gmail.com>
2020-05-04 06:11:33 +0100

    feat(dashboard modification 002-bug fixed)

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import UserHeader from '../../components/UserHeader/index';
 import UserSidebar from '../../components/UserSidebar/index';
 import Back_Button from '../../images/Chevron.svg';
-import Footer from '../../components/Footer/index'
+import UserFooter from '../../components/UserFooter/index'
 import './dashboard.css';
 
 function Dashboard() {
