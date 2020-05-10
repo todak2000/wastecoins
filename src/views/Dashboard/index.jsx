8fef7f54c7a04ec0028d6af755b253237fd2ba46
Daniel Olagunju <todak2000@gmail.com>
2020-05-10 03:24:59 +0100

    Update index.jsx

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import UserHeader from '../../components/UserHeader/index';
 import UserSidebar from '../../components/UserSidebar/index';
-import Back_Button from '../../images/Chevron.svg';
 import UserFooter from '../../components/UserFooter/index'
 import './dashboard.css';
 
